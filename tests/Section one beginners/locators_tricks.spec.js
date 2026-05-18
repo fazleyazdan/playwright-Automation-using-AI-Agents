139e447f65cb5e795e8638201569b0d11acfb360
fazleyazdan <fazleyazdan345@gmail.com>
2026-05-18 18:41:16 +0500

    locator tricks

--- tests/Section one beginners/locators_tricks.spec.js
+++ tests/Section one beginners/locators_tricks.spec.js
@@ -8,17 +8,26 @@ import {test, expect} from "@playwright/test"
 
 test("locators & assertions", async({page}) => {
 
+
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
     await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy")
     await page.fill("#username", "rahulshettyacademy")
     await page.fill("#password", "wrongpass")
     await page.click("input[type='submit']")
 
-//  const validation_text = await page.locator("[style*='display']").textContent()    // this locator looks for partial value match of styel attrib
+//  const validation_text = await page.locator("[style*='display']").textContent()    // this locator looks for partial value match of style attrib
 //  await expect(validation_text).toContain("Incorrect") 
 //  OR
     await expect(page.locator("[style='display: block;']")).toContainText("Incorrect")
 
+//  We Entered wrong credentials, Now we want to enter corret
+//  since the input boxes at this stage already contains some values. to remove this use fill() & pass empty string
 
+    await page.fill("#password", "")
+    await page.fill("#password", "Learning@830$3mK2")
+    await page.click("input[type='submit']")
+
+//  Here if you notice we are using same locators again and again. you can store it in a variable and use it further in your tests
+//  There is a desing pattern called POM. which i will use in future tests
 
 })
