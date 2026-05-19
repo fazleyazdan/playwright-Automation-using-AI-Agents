b85a23591148fa014527b6554c32ad964ae10db4
fazleyazdan <fazleyazdan345@gmail.com>
2026-05-19 23:53:09 +0500

    extract multi web elements

--- tests/Section one beginners/extract_multi_webelement.spec.js
+++ tests/Section one beginners/extract_multi_webelement.spec.js
@@ -4,7 +4,8 @@ import {test, expect} from "@playwright/test"
 test("extract multi web elements", async({page}) => {
 
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
-    
+
     // first store reusable locators
+    
     
 })
