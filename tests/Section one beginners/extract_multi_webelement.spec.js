6bc38b1221a28db468f3e33415a0f7ba21b5a8f5
fazleyazdan <fazleyazdan345@gmail.com>
2026-05-28 23:15:03 +0500

    working with multiple locators

--- tests/Section one beginners/extract_multi_webelement.spec.js
+++ tests/Section one beginners/extract_multi_webelement.spec.js
@@ -6,6 +6,6 @@ test("extract multi web elements", async({page}) => {
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
 
     // first store reusable locators
-
+    
     
 })
