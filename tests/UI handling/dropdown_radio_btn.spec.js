81fb4bcdbc9b3838f83343db8e4b74ae1d4fde06
fazleyazdan <fazleyazdan345@gmail.com>
2026-06-17 15:44:15 +0500

    handling dropdowns

---
+++ tests/UI handling/dropdown_radio_btn.spec.js
@@ -0,0 +1,27 @@
+import {test, expect} from "@playwright/test"
+
+
+test("Handling dropdowns & radio buttons", async({page}) => {
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+
+/** 
+- Handling Dropdowns
+  First capture the dropdown & then you can manipulate it to select Specific Options
+
+- Once the dropdown element is captured, you can then use playwright method selectOption 
+  to select specific option by value attribute
+
+- Note that dropdown elements most of the times is using "select" tag, then the options
+  are using option "tag". option tag contains value attribute through which we can select individual
+  dropdown options using "selectOption" Method
+
+  **/
+
+    const dropdown_element = await page.locator("select.form-control")
+    await dropdown_element.selectOption("teach")
+    await page.pause()
+
+
+
+})
